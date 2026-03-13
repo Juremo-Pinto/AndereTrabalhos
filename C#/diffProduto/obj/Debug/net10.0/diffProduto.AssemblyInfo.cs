0267c2e37d89c206b42b149d9ef3f23d77313f1e
Juremo-Pinto <fredericoteodoroarantes@gmail.com>
2026-03-13 11:36:40 -0300

    aa

--- C#/diffProduto/obj/Debug/net10.0/diffProduto.AssemblyInfo.cs
+++ C#/diffProduto/obj/Debug/net10.0/diffProduto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("diffProduto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e1cc50392c2aa9318635f5144f41b3da1d5641")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c279ee3631f4ef29b9700bd1fdbf71c7b3bfd3a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("diffProduto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("diffProduto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
