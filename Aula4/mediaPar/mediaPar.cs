11a9c02138903758adbfb7bec8ab396c14956ef2
Juremo-Pinto <fredericoteodoroarantes@gmail.com>
2026-03-24 10:51:03 -0300

    Something changed and i know what it is but i wont tell you

---
+++ Aula4/mediaPar/mediaPar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.NetworkInformation;
+namespace Tabuada
+{
+    class Programinha
+    {
+        static void falar(string m) => Console.Write(m);
+        static int ouvirP() => int.Parse(Console.ReadLine());
+        static double ouvir2() => Convert.ToDouble(Console.ReadLine());
+        static void Main()
+        {
+            falar("Defina a quantidade de numeros da sequencia.");
+            int quant = ouvirP();
+            
+        }
+    }
+}
