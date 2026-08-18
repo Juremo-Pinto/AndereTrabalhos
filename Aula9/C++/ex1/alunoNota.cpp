757df562303a7154ba51355006441d0fbd20ee7b
Juremo-Pinto <fredericoteodoroarantes@gmail.com>
2026-08-18 14:10:57 -0300

    Aula9
    listas

--- Aula9/C++/ex1/alunoNota.cpp
+++ Aula9/C++/ex1/alunoNota.cpp
@@ -10,7 +10,17 @@ using namespace std;
 int main()
 {
 
-    list<int> grades;
+    SetConsoleOutputCP(65001);       // permite que o terminal mostre caracteres utf-8 corretamente
+    setlocale(LC_ALL, "pt_BR.utf8"); // permite usar acentuação e interpretar caracteres utf-8
 
-    
+    list<int> grades;
+    list<string> students = {"Marcos", "Paulo", "Astolfo", "Pedro", "Arantes"};
+
+    int grade;
+    cin >> grade;
+
+    grades.push_back(grade);
+
+    auto it = grades.begin();
+    cout << *it << endl;
 }
